8c8d9db508545bc5a9360811f0a733e241ae0d5a
tommysems <tomsems@gmail.com>
2023-05-30 15:53:41 -0400

    Added some interacting with Javasccript

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+
+//function othername()
+//{
+  //  var input= document.getElementById("userInput");
+    //alert(input);
+//}
+
+document.getElementById("Netflix").addEventListener("click", () => {
+    const a = document.getElementById("Netflix").textContent;
+    alert(`Are you sure you want to delete ${a} from blocklist?`);
+});
