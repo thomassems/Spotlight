373bfd61295fd42fd499390a11061f78d66f5bb3
tommysems <tomsems@gmail.com>
2023-05-31 09:14:24 -0400

    Worked on the form

--- scripts/main.js
+++ scripts/main.js
@@ -7,3 +7,17 @@ document.querySelectorAll('li').forEach(item => {
         }
     })
 });
+
+const text = document.getElementById("userInput");
+const btn = document.getElementById("buttonClick");
+function addWebsite() {
+    var node = document.createElement('li');
+    node.appendChild(document.createTextNode(text.value));
+    document.querySelector('ul').appendChild(node);
+    const output = document.getElementById("output");
+    output.innerHTML = `${text.value} was successfully added to the website block list`;
+};
+btn.addEventListener("click", addWebsite);
+
+
+/* need to remove the item from local storage too*/
