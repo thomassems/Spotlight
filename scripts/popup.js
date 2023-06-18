d717bfa8e7def2b179f10e1544c0a9fdf9658369
tommysems <tomsems@gmail.com>
2023-06-17 21:30:05 -0400

    Added 2 places before and after the decimal for the timer and worked on re-adding the arrow buttons when the timer finishes

--- scripts/popup.js
+++ scripts/popup.js
@@ -2,31 +2,63 @@ const left = document.getElementById("leftArrow");
 const time = document.getElementById("time");
 const right = document.getElementById("rightArrow");
 const logoButton = document.getElementById("logoButton");
-let accTime = Number(time.textContent.split(":")[0]);
+let mins = Number(time.textContent.split(":")[0]);
+let secs = Number(time.textContent.split(":")[1]);
 left.addEventListener("click", leftUpdater);
 right.addEventListener("click", rightUpdater);
 logoButton.addEventListener("click", startTimer)
 
 function leftUpdater() {
-    if (time.textContent != "10:00") {
-        time.innerHTML = `${accTime - 5}:00`;
-        accTime -= 5;
+    if (time.textContent != "11:00") {
+        time.innerHTML = `${mins - 1}:00`; // should be mins - 5, just changed it for testing purposes
+        mins -= 1; // mins -= 5
     }
 }
 
 function rightUpdater() {
-    if (time.textContent != "180:00") {
-        time.innerHTML = `${accTime + 5}:00`;
-        accTime += 5;
+    if (time.textContent != "120:00") {
+        time.innerHTML = `${mins + 5}:00`;
+        mins += 5;
     }
 }
 
 function startTimer () {
-    left.innerHTML = "";
-    right.innerHTML = "";
+    left.parentNode.removeChild(left);
+    right.parentNode.removeChild(right);
+    countDown(new Date().getTime());
+}
+
+function countDown(startTime) {
+    setInterval(function() {
+        let currTime = new Date().getTime();
+        let distance = currTime - startTime;
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        const timerMins = mins - minutes - 1;
+        const timerSecs = secs - seconds + 59;
+        if (timerSecs < 10) {
+            time.innerHTML = `${timerMins}:0${timerSecs}`; 
+        }
+        else if (timerMins < 10) {
+            time.innerHTML = `0${timerMins}:${timerSecs}`;
+        }
+        else {
+            time.innerHTML = `${timerMins}:${timerSecs}`;
+        }
+
+        if (timerMins < 0) {
+            time.innerHTML = `${mins}:${secs}`;
+            clearInterval(countDown());
+            let leftAr = document.createElement("button");
+            let right = document.createElement("button");
+            leftAr.innerHTML = '&#8249';
+            left.appendChild(leftAr);
+            right.innerHTML = '&#8250';
+        }
+})
 }
 
 
-
-// also we will have to add an event listener for when the logo is clicked which causes the arrow buttons to dissappear and for a countdown to appear for a limited
-// amount of time which activates the extension
+// when logo is clicked, the extension should be activated, and it should remain active as you flip tabs
+// once the timer ends, should add a popup message saying congrats, and then when they click ok
+// also add a 0 before the digits if they are less then 10
