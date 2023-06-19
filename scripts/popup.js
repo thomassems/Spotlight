2103d43df0e343cbaceda1326ff5b857aef986cf
tommysems <tomsems@gmail.com>
2023-06-19 11:06:34 -0400

    Simplified the timer function

--- scripts/popup.js
+++ scripts/popup.js
@@ -23,41 +23,44 @@ function rightUpdater() {
 }
 
 function startTimer () {
+    let startingTime = new Date().getTime();
     left.parentNode.removeChild(left);
     right.parentNode.removeChild(right);
-    countDown(new Date().getTime());
+    const interval = setInterval(function() { timer(startingTime, interval); });
 }
 
-function countDown(startTime) {
-    setInterval(function() {
-        let currTime = new Date().getTime();
-        let distance = currTime - startTime;
-        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-        const timerMins = mins - minutes - 1;
-        const timerSecs = secs - seconds + 59;
-        if (timerSecs < 10) {
-            time.innerHTML = `${timerMins}:0${timerSecs}`; 
-        }
-        else if (timerMins < 10) {
-            time.innerHTML = `0${timerMins}:${timerSecs}`;
-        }
-        else {
-            time.innerHTML = `${timerMins}:${timerSecs}`;
-        }
+function timer(startTime, intervalVar) {
+    let currTime = new Date().getTime();
+    let distance = currTime - startTime;
+    const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    const timerMins = mins - minutes - 1;
+    const timerSecs = secs - seconds + 59;
+    if (timerSecs < 10) {
+        time.innerHTML = `${timerMins}:0${timerSecs}`; 
+    }
+    else if (timerMins < 10) {
+        time.innerHTML = `0${timerMins}:${timerSecs}`;
+    }
+    else {
+        time.innerHTML = `${timerMins}:${timerSecs}`;
+    }
 
-        if (timerMins < 0) {
-            time.innerHTML = `${mins}:${secs}`;
-            clearInterval(countDown());
-            let leftAr = document.createElement("button");
-            let right = document.createElement("button");
-            leftAr.innerHTML = '&#8249';
-            left.appendChild(leftAr);
-            right.innerHTML = '&#8250';
-        }
-})
+    if (timerMins < 0) {
+        clearTimer(mins, intervalVar);
+    }
 }
 
+function clearTimer(mins, intervalVar) {
+    time.innerHTML = `${mins}:${secs}`;
+    let leftAr = document.createElement("button");
+    let rightAr = document.createElement("button");
+    leftAr.innerHTML = '&#8249';
+    left.appendChild(leftAr);
+    rightAr.innerHTML = '&#8250';
+    right.appendChild(rightAr);
+    clearInterval(intervalVar);
+}
 
 // when logo is clicked, the extension should be activated, and it should remain active as you flip tabs
 // once the timer ends, should add a popup message saying congrats, and then when they click ok
